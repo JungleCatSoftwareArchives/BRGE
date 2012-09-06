e3bcfd980e854c9c57fecff87f2d218d67d172a6
Bryan Wyatt <brwyatt@brwyatt.net>
2012-09-06 00:04:35 -0500

    Moved the start sequence
    This will allow BRGE to be called/started by another package.

--- brwyatt/brge/BRGE.java
+++ brwyatt/brge/BRGE.java
@@ -8,21 +8,25 @@ public class BRGE { //Main class
 	private static int height=600;
 	private static GameWindow gw;
 	private static ScreenObjects screenObjects;
-	private static Game game;
+	private static Game theGame;
 	
 	public static void main(String args[]){
-		screenObjects=new ScreenObjects();
-		game=new TestGame(screenObjects); //change this to whichever game you want!
-		gw=new GameWindow(screenObjects);
-		(new Thread(gw)).start(); //Load GameWindow and start rendering
-		
-		game.startGame();//start the game!
+		start(new TestGame(screenObjects)); //change this to whichever game you want!
 	}
 	
-	
+	public static void start(Game game){
+		if(theGame==null){ //only start once
+			screenObjects=new ScreenObjects();
+			theGame=game;
+			gw=new GameWindow(screenObjects);
+			(new Thread(gw)).start(); //Load GameWindow and start rendering
+			
+			theGame.startGame();//start the game!
+		}
+	}
 	
 	public static String getGameTitle(){
-		return game.getGameTitle();
+		return theGame.getGameTitle();
 	}
 	public static int getWidth(){
 		return width;
@@ -31,13 +35,13 @@ public class BRGE { //Main class
 		return height;
 	}
 	public static void keyPressed(int key){
-		game.keyPressed(key);
+		theGame.keyPressed(key);
 	}
 	public static void keyReleased(int key){
-		game.keyReleased(key);
+		theGame.keyReleased(key);
 	}
 	public static void keyTyped(int key){
-		game.keyTyped(key);
+		theGame.keyTyped(key);
 	}
 	public static void exit(){
 		//cleanup and exit
