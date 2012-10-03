c832a6775cd9f64f237557332f7d3434dfa429a4
Bryan Wyatt <brwyatt@brwyatt.net>
2012-10-02 21:35:27 -0500

    Adding LGPL

--- src/net/brwyatt/brge/graphics/drawables/BlackBackground.java
+++ src/net/brwyatt/brge/graphics/drawables/BlackBackground.java
@@ -1,4 +1,23 @@
-package net.brwyatt.brge.graphics.drawables;
+/* 
+ * Copyright 2012 Bryan Wyatt
+ * 
+ * This file is part of BRGE.
+ *  
+ * BRGE is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * BRGE is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with BRGE.  If not, see <http://www.gnu.org/licenses/>.
+ */
+ 
+ package net.brwyatt.brge.graphics.drawables;
 
 import java.awt.Color;
 import java.awt.Graphics;
